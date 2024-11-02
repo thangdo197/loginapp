61d6978b5650c305e1ffb2d4d7803f47e0801b1e
thangdo197 <thangdo190702@gmail.com>
2024-11-02 08:26:20 +0700

    first commit

---
+++ app/WelcomeScreen.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { ImageBackground, StyleSheet, Text } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+
+const image = { uri: "https://legacy.reactjs.org/logo-og.png" };
+
+const App = () => (
+  <SafeAreaProvider>
+    <SafeAreaView style={styles.container} edges={["left", "right"]}>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        <Text style={styles.text}>Inside</Text>
+      </ImageBackground>
+    </SafeAreaView>
+  </SafeAreaProvider>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    lineHeight: 84,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#000000c0",
+  },
+});
+
+export default App;
